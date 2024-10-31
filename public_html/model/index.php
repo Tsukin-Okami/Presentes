0a7e6cca1ebc63bdf0103db7ff032395db07f1b5
Tsukin-Okami <jhonikauanmb@gmail.com>
2024-10-31 13:43:58 -0300

    Update index.php

--- public_html/model/index.php
+++ public_html/model/index.php
@@ -3,14 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index Php</title>
 </head>
 <body>
+    <h1>Php Model Test</h1><hr>
     <?php
         include './connection.php';
         include './usuario.php';
         include './produto.php';
         include './lista.php';
+
+        //var_dump(Connection::getConnection());
 
         $u = new Usuario;
         //$u->addUsuario('a@a', 'a', 'a');
@@ -20,19 +23,16 @@
 
         $produto = new Produto;
         //$s = $produto->recebeProdutos();
-        //var_dump($s);
         //$produto->addProduto("Banana", "Um fruto amarelo");
+        //var_dump($s);
 
         $lista = new Lista;
         //$s = $lista->addLista("a@a", "descrição da lista");
-        //var_dump($s);
-        //$lista->removeLista("a@a");
         //$s = $lista->getLista("a@a");
-        //var_dump($s);        
+        //$lista->removeLista("a@a");
         //$s = $lista->addItem("a@a", 1);
-        $lista->removeItem(3, 1);
-
-        //var_dump(Connection::getConnection());
+        //$lista->removeItem(3, 1);
+        //var_dump($s);
     ?>
 </body>
 </html>
